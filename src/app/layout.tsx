0cd2884aa3ad74dd41de635cf6f17aa5da2fd8c5
rutasdutas-prog <rutasdutas@gmail.com>
2026-07-04 23:45:24 +0700

    "Fix mobile UI"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,13 @@ export const metadata: Metadata = {
   description: "Buat toko online profesional dalam hitungan menit. Kelola produk, inventaris, pesanan, dan pembayaran dengan mudah.",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
